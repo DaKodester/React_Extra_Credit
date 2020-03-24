17c215da98cb8ed455bea694fbbce92d97e0b35d
DaKodester <dricodp@dunwoody.edu>
2020-03-24 16:41:01 -0500

    Submitting extra credit

---
+++ src/Suggestions.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Suggestions() {
+  return (
+    <section id="profile_suggestions">
+       <h3>Suggestions</h3>
+    </section>
+  );
+}
+
+export default Suggestions;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Offers from './Offers';
 import * as serviceWorker from './serviceWorker';
+import Suggestions from './Suggestions';
+import Task from './Task';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Offers />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('Offers')
+);
+ReactDOM.render(
+  <React.StrictMode>
+    <Suggestions />
+  </React.StrictMode>,
+  document.getElementById('Suggestions')
+);
+ReactDOM.render(
+  <React.StrictMode>
+    <Task />
+  </React.StrictMode>,
+  document.getElementById('Task')
 );
 
 // If you want your app to work offline and load faster, you can change
